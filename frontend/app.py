49e9d3bc1654a5ee055be720e86fffbd3a044c90
tiqfds <87251702+tiqfds@users.noreply.github.com>
2024-02-18 15:51:01 -0800

    added function

--- frontend/app.py
+++ frontend/app.py
@@ -1,15 +1,15 @@
 import pandas
-import streamlit as st
+import streamlit
 import pickle
 import xgboost
 import os
 import requests
 import json
 
-st.title("Credit Card Fraud Detection App")
+streamlit.title("Credit Card Fraud Detection App")
 
 
-st.write("""
+streamlit.write("""
          Credit card fraud is a type of identity theft that happens when someone uses your credit card information, **without your permission**, to make unwanted purchases.
          They don't need your physical card, just the details like the number, expiration date, and security code (CCV).
          
@@ -20,4 +20,29 @@ st.write("""
          
          """)
 
-date_range = st.sidebar.text_input("""Choose a date range""")
+def run():
+    
+    quarter = streamlit.sidebar.selectbox('Which quarter do you want to analyze?', (1, 2, 3, 4))
+    if quarter == 1:
+        q_data = '../data/processed/web_app_data/Q1.feather'
+    if quarter == 2:
+        q_data = '../data/processed/web_app_data/Q2.feather'
+    if quarter == 3:
+        q_data = '../data/processed/web_app_data/Q3.feather'
+    if quarter == 4:
+        q_data = '../data/processed/web_app_data/Q4.feather'
+
+    @streamlit.cache_data
+    def load_data(q_data):
+        df = pandas.read_feather(q_data)
+        return df
+    
+    df = load_data(q_data)
+
+    if streamlit.sidebar.checkbox('Show information about the data'):
+        streamlit.write(df.head(10))
+        streamlit.write("{0} transactions loaded as training data, containing {1} fraudulent transactions".format(len(df),df.TX_FRAUD.sum()))
+
+
+if __name__ == '__main__':
+    run()
